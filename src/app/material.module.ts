a0c08d225197c141bb75437156b1e9588057aa3e
Miguel Caravantes <miguel.caravantes0@gmail.com>
2020-04-24 21:54:39 -0500

    add plus button to dashboard

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,7 +3,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
-
 @NgModule({
   declarations: [],
   imports: [
