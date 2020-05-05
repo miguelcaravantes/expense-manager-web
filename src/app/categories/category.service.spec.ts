4477de1613c6d05a4ba24e8cdd3b892853a44fb0
hector saavedra <hector.saavedra.r@outlook.com>
2020-05-04 18:12:17 -0600

    Add Import HttpClientModule

--- src/app/categories/category.service.spec.ts
+++ src/app/categories/category.service.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+
 
 import { CategoryService } from './category.service';
 
@@ -6,7 +8,7 @@ describe('CategoryService', () => {
   let service: CategoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports:[HttpClientModule]});
     service = TestBed.inject(CategoryService);
   });
 
