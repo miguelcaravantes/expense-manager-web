c96afc337017d50fba97445a48b3edf274710b04
Miguel <miguel.caravantes0@gmail.com>
2020-09-04 13:48:26 -0500

    FormValidationService Cleanup

--- src/app/core/forms/form-validation.service.ts
+++ src/app/core/forms/form-validation.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { NgControl, AbstractControl, ValidationErrors } from '@angular/forms';
 
+
+const defaultMessages: { [key: string]: string } = {
+  required: '[name] is required',
+  email: 'The email format is not valid',
+  minlength: 'The minimun length is {requiredLength}',
+  maxlength: 'The maximun length is {requiredLength}',
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,49 +19,31 @@ export class FormValidationService {
   processErrors(ngControl: NgControl): string {
     const { control, errors } = ngControl;
     const join = (p: string, c: string) => `${p}. ${c}`;
-
-    if (control) {
-      if (errors) {
-        return Object.keys(errors)
-          .map(key => this.getMessages(control, errors, key))
-          .reduce(join);
-      }
-    }
-    return '';
+    return control && errors ?
+      Object.keys(errors)
+        .map(key => this.getMessages(control, errors, key))
+        .reduce(join) : '';
   }
 
   getMessages(control: AbstractControl, errors: ValidationErrors, errorKey: string): string {
-    const error = errors[errorKey];
+    const errorPayload = errors[errorKey];
     const controlMessages = this.getControlMessages(control);
-    let result = controlMessages[errorKey];
-    if (!result) {
-      result = defaultMessages[errorKey];
-    }
-    if (result) {
-      result = this.processMessage(result, error);
-    }
-    return result;
+    const errorMessage = controlMessages[errorKey] ?? defaultMessages[errorKey];
+
+    return this.processMessage(errorMessage, errorPayload);
   }
 
   getControlMessages(control: AbstractControl): { [key: string]: string } {
     return Reflect.get(control, 'validationMessages') || {};
   }
 
-  private processMessage(message: string, error: { [key: string]: string }): string {
+  private processMessage(message: string, errorPayload: { [key: string]: string }): string {
     return message.replace(/{.*?}/g, (match, _) => {
       const prop = match.replace(/{|}/g, '');
-      return error[prop];
+      return errorPayload[prop];
     });
   }
-
-
 }
 
 
 
-const defaultMessages: { [key: string]: string } = {
-  required: '[name] is required',
-  email: 'The email format is not valid',
-  minlength: 'The minimun length is {requiredLength}',
-  maxlength: 'The maximun length is {requiredLength}',
-};
